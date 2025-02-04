1f1b4290b6d1926d827947f4f54a4c0c7a46dcab
xlenco <1043865083@qq.com>
2025-02-04 09:18:19 +0800

    update: personal information

--- app/app.config.ts
+++ app/app.config.ts
@@ -5,8 +5,8 @@ export default defineAppConfig({
     ...homepageConfig,
 
     footer: {
-        copyright: `© ${new Date().getFullYear()} 纸鹿本鹿`,
-        message: 'aka Zhilu, L33Z22L11',
+        copyright: `© ${new Date().getFullYear()} 希乐`,
+        message: 'aka xlenco, 希乐',
     },
 
     nav: <Nav>[
@@ -17,17 +17,17 @@ export default defineAppConfig({
                 { icon: 'ph:pen-nib-duotone', text: '文章', url: '/article' },
                 // { icon: "ph:pen-nib-duotone", text: "文章", url: "https://blog.zhilu.cyou", external: true },
                 // { icon: "ph:code-duotone", text: "项目", url: "/project" },
-                { icon: 'ph:code-duotone', text: '项目', url: 'https://github.com/L33Z22L11/#user-33976233-pinned-items-reorder-form', external: true },
-                { icon: 'ph:globe-duotone', text: '站点', url: '/site' },
-                { icon: 'ph:files-duotone', text: '日志', url: '/log' },
+                // { icon: 'ph:code-duotone', text: '项目', url: 'https://github.com/L33Z22L11/#user-33976233-pinned-items-reorder-form', external: true },
+                // { icon: 'ph:globe-duotone', text: '站点', url: '/site' },
+                // { icon: 'ph:files-duotone', text: '日志', url: '/log' },
             ],
-        },
+        },    
         {
             title: '社交',
             items: [
-                { icon: 'basil:qq-outline', text: '群: 169994096', url: 'https://jq.qq.com/?_wv=1027&k=lQfNSeEd', external: true },
-                { icon: 'ph:github-logo-duotone', text: 'Github', url: 'https://github.com/L33Z22L11', external: true },
-                { icon: 'ph:telegram-logo-duotone', text: 'Telegram', url: 'https://t.me/L33Z22L11', external: true },
+                // { icon: 'basil:qq-outline', text: '群: 169994096', url: 'https://jq.qq.com/?_wv=1027&k=lQfNSeEd', external: true },
+                { icon: 'ph:github-logo-duotone', text: 'Github', url: 'https://github.com/xlenco', external: true },
+                // { icon: 'ph:telegram-logo-duotone', text: 'Telegram', url: 'https://t.me/L33Z22L11', external: true },
             ],
         },
     ],
